085df5531313f1395c8fbece904d6b8d3b98b544
Laurianne Gerin <laurianne.gerin@gmail.com>
2016-08-14 16:46:17 +0200

    Finished music model, controller and views. Installation of figaro

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -1,26 +1,49 @@
 class MusicsController < ApplicationController
 
   def index
-
+    @musics = Music.all
   end
 
   def show
+    @music = Music.find(params[:id])
   end
 
   def new
-
+    @music = Music.new
   end
 
   def create
+    @music = Music.new(music_params)
+    if @music.save
+      redirect_to music_path(@music.id)
+    else
+      render :new
+    end
   end
 
   def edit
+    @music = Music.find(params[:id])
   end
 
   def update
+    @music = Music.find(params[:id])
+    if @music.update(music_params)
+      redirect_to music_path(@music.id)
+    else
+      render :edit
+    end
   end
 
-  def delete
-
+  def destroy
+    @music = Music.find(params[:id])
+    @music.destroy
+    redirect_to musics_path
   end
+
+  private
+
+  def music_params
+    params.require(:music).permit(:title, :artist, :album, :year)
+  end
+
 end
--- app/models/music.rb
+++ app/models/music.rb
@@ -1,2 +1,5 @@
 class Music < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :artist, presence: true
+  validates :album, presence: true, uniqueness: true
 end
